f6a727973cb2468f7bf6ccb78e2e45402f194430
rodrigobutta <rbutta@gmail.com>
2019-12-21 16:12:37 -0300

    ReactNativerNavigation Android

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import {Navigation} from 'react-native-navigation';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('navigation.playground.WelcomeScreen', () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'navigation.playground.WelcomeScreen',
+      },
+    },
+  });
+});
